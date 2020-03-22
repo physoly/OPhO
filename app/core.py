0520e3cc8347d56f12bffc7599679ca6ebd662d8
Epsilon10 <benjamin.oommen1@gmail.com>
2020-03-21 22:08:51 -0500

    add template engine

--- app/core.py
+++ app/core.py
@@ -1,14 +1,14 @@
-from sanic import Sanic, response
-from sanic.response import html, text, json, HTTPResponse, redirect
-
-from sanic_session import Session, InMemorySessionInterface
-
 from app import app
 
+from sanic import Sanic, response
+from sanic.response import html, text, json, HTTPResponse, redirect
+from sanic_session import Session, InMemorySessionInterface
+
 import ujson
-
 import aiohttp
+from jinja2 import Environment, PackageLoader
 
+env = Environment(loader=PackageLoader('app', 'templates'))
 session_interface = Session(app, interface=InMemorySessionInterface())
 
 app.config.AUTH_LOGIN_ENDPOINT = 'login'
@@ -17,6 +17,18 @@ with open("./config/config.json") as f:
     global_config = ujson.loads(f.read())
 
 app.config.SECRET_KEY = global_config['SECRET_KEY']
+
+def template(tpl, *args, **kwargs):
+    template = env.get_template(tpl)
+    request = get_stack_variable('request')
+    user = None
+    if request['session'].get('logged_in'):
+        user = request['session']['user']
+    kwargs['request'] = request
+    kwargs['session'] = request['session']
+    kwargs['user'] = user
+    kwargs.update(globals())
+    return html(template.render(*args,**kwargs))
 
 @app.listener('before_server_start')
 async def server_begin(app, loop):
