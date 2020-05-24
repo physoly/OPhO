a342f834dba6944db1536187e6c3cb8a0c8b00c2
Epsilon10 <benjamin.oommen1@gmail.com>
2020-05-23 23:22:48 -0500

    actually update

---
+++ scripts/create_test_user.py
@@ -0,0 +1,24 @@
+from utils import run_async, get_connection
+
+async def initialize_team(teamname, password, problem_number):
+    conn =  await get_connection()
+    insert_and_return = f"""
+        INSERT INTO user_details(username, password) VALUES ('{teamname}', '{password}') RETURNING user_id
+    """
+    
+    team_id = await conn.fetchval(insert_and_return)
+
+    print("TEAM ID:", team_id)
+
+    create_table = f"""
+        CREATE TABLE team{team_id}(problem_no integer references problems,solved BOOLEAN NOT NULL, attempts integer, answers decimal[], timestamp timestamp);
+    """
+    insert_query = f"""
+        INSERT INTO team{team_id} (problem_no, solved, attempts) VALUES """ + ', '.join(f"({number}, FALSE, 0)" for number in range(1, problem_number+1)) + ";"
+    
+    insert_rankings = f"""INSERT INTO rankings(team_id, problems_solved) VALUES ($1, 0)"""
+    await conn.execute(create_table)
+    await conn.execute(insert_query)
+    await conn.execute(insert_rankings, team_id)
+
+run_async(initialize_team("TESTUSER3", "IREADMORIN2", 55))
