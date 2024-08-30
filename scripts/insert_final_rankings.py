9798c42875957002fa368abb61d84233e1739b77
Ashmit Dutta <ashmit.dutta105@gmail.com>
2024-08-30 09:32:56 -0500

    done

--- scripts/insert_final_rankings.py
+++ scripts/insert_final_rankings.py
@@ -4,13 +4,16 @@ from utils import get_connection, run_async
 async def execute():
     root = "INSERT INTO rankings_2024(team_id, score) VALUES "   
     entries = [] 
-    with open('../data/2023/rankings.csv', 'r') as csvin:
-        for line in csv.reader(csvin):
+    with open('/Users/ashmitdutta/OPhO/scripts/data/2024_invi_qualifiers.csv', 'r') as csvin:
+        reader = csv.reader(csvin)
+        next(reader)  # Skip the header row
+        for line in reader:
             team_id = line[0]
-            score = round(float(line[2]), 3)
+            score = round(float(line[1]), 3)  # Corrected index to line[1]
             entries.append(f"({team_id}, {score})")
     query = root + ','.join(entries)
     conn = await get_connection()
     print(query)
     await conn.execute(query)
-run_async(execute()) 
+
+run_async(execute())
